9025bad3b8b5fe6f3184c6f17e7c8ef84a8849a7
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-12-22 12:58:55 +0900

    fix

--- src/main.py
+++ src/main.py
@@ -18,6 +18,6 @@ elif float(data["totals"]["percent_covered"]) < threshold_acceptable:
 else:
     quality = ':tada: :sunglasses: :tada: :sunglasses: :tada: :sunglasses: :tada: :sunglasses:'
 
-string_content = f'{quality}\n## Total coverage: {data["totals"]["percent_covered"]:.2f}\n{quality}'
+string_content = f'{quality} ## Total coverage: {data["totals"]["percent_covered"]:.2f}'
 
 print(f'::set-output name=result::{string_content}')
