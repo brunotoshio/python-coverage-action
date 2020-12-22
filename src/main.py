8aabd20018fd705156cd9cae39a7539e5d72561a
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-12-22 12:43:58 +0900

    adding emoticons

--- src/main.py
+++ src/main.py
@@ -5,9 +5,19 @@ import os
 
 file_path = os.getenv('INPUT_FILEPATH')
 
+threshold_bad = float(os.getenv('INPUT_THRESHOLD_BAD', 70))
+threshold_acceptable = float(os.getenv('INPUT_THRESHOLD_ACCEPTABLE', 90))
+
 with open(os.path.join(os.getcwd(), file_path), 'r') as f:
     data = json.load(f)
 
-string_content = f'## Total coverage: {data["totals"]["percent_covered"]:.2f}'
+if float(data["totals"]["percent_covered"]) < threshold_bad:
+    quality = ':rotating_light: :scream: :rotating_light: :scream: :rotating_light: :scream: :rotating_light: :scream:'
+elif float(data["totals"]["percent_covered"]) < threshold_acceptable:
+    quality = ':warning: :thinking: :warning: :thinking: :warning: :thinking: :warning: :thinking:'
+else:
+    quality = ':tada: :sunglasses: :tada: :sunglasses: :tada: :sunglasses: :tada: :sunglasses:'
+
+string_content = f'{quality}\n## Total coverage: {data["totals"]["percent_covered"]:.2f}\n{quality}'
 
 print(f'::set-output name=result::{string_content}')
