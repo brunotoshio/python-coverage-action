6aff1cc103f705d9e28a3faea7f8d57c6430828e
Bruno Toshio Sugano <brunotoshio@gmail.com>
2020-12-24 13:15:46 +0900

    passed or not

--- src/main.py
+++ src/main.py
@@ -1,23 +1,49 @@
+from typing import Tuple
 import json
 import logging
 import os
 
 
-file_path = os.getenv('INPUT_FILEPATH')
+def generate_md_string(
+        filepath: str,
+        th_bad: float = 70,
+        th_acceptable: float = 90
+    ) -> Tuple[float, str]:
+    """Generates a markdown string from a coverage JSON file
 
-threshold_bad = float(os.getenv('INPUT_THRESHOLD_BAD', 70))
-threshold_acceptable = float(os.getenv('INPUT_THRESHOLD_ACCEPTABLE', 90))
+    :param filepath: The path to the file
+    :type filepath: str
+    :param th_bad: Upper limit for a bad coverage, defaults to 70
+    :type th_bad: float, optional
+    :param th_acceptable: Upper limit for a acceptable coverage, defaults to 90
+    :type th_acceptable: float, optional
+    :return: The current coverage and the markdown string
+    :rtype: Tuple[float, str]
+    """
+    with open(os.path.join(os.getcwd(), filepath), 'r') as f:
+        data = json.load(f)
 
-with open(os.path.join(os.getcwd(), file_path), 'r') as f:
-    data = json.load(f)
+    current_coverage = float(data["totals"]["percent_covered"])
+    if current_coverage < th_bad:
+        quality = ':rotating_light: :scream: :rotating_light: :scream: :rotating_light: :scream: :rotating_light: :scream:'
+    elif current_coverage < th_acceptable:
+        quality = ':warning: :thinking: :warning: :thinking: :warning: :thinking: :warning: :thinking:'
+    else:
+        quality = ':tada: :sunglasses: :tada: :sunglasses: :tada: :sunglasses: :tada: :sunglasses:'
 
-if float(data["totals"]["percent_covered"]) < threshold_bad:
-    quality = ':rotating_light: :scream: :rotating_light: :scream: :rotating_light: :scream: :rotating_light: :scream:'
-elif float(data["totals"]["percent_covered"]) < threshold_acceptable:
-    quality = ':warning: :thinking: :warning: :thinking: :warning: :thinking: :warning: :thinking:'
-else:
-    quality = ':tada: :sunglasses: :tada: :sunglasses: :tada: :sunglasses: :tada: :sunglasses:'
+    return current_coverage, f'## Total coverage: {data["totals"]["percent_covered"]:.2f} {quality}'
 
-string_content = f'## Total coverage: {data["totals"]["percent_covered"]:.2f} {quality}'
 
-print(f'::set-output name=result::{string_content}')
+if __name__ == '__main__':
+    file_path = os.getenv('INPUT_FILEPATH')
+    threshold_bad = float(os.getenv('INPUT_THRESHOLD_BAD', 70))
+    threshold_acceptable = float(os.getenv('INPUT_THRESHOLD_ACCEPTABLE', 90))
+    threshold_pass = float(os.getenv('INPUT_THRESHOLD_PASS', 70))
+    cov, md_output = generate_md_string(
+        file_path,th_bad=threshold_bad, th_acceptable=threshold_acceptable)
+
+    print(f'::set-output name=result::{md_output}')
+    if cov >= threshold_pass:
+        print(f'::set-output name=passed::true')
+    else:
+        print(f'::set-output name=passed::false')
